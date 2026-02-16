83af589bf1248b08a09763bc49c690228db6b367
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2026-02-16 11:58:32 +0000

    Making Touchmanager
    Managing different types of touch event and their outputs.

---
+++ Assets/Scripts/Input/Touch/TouchManager.cs
@@ -0,0 +1,34 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+   private PlayerInput playerInput;
+
+   private InputAction touchPositionAction;
+   private InputAction touchPressAction;
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPositionAction = playerInput.actions["TouchPosition"];
+        touchPressAction = playerInput.actions["TouchPress"];
+    }
+
+
+    private void OnEnable()
+    {
+        touchPressAction.performed += TouchPressed;
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+    }
+
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+        Debug.Log(value);
+    }
+}
